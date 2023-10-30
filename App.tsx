620bd0ce7faa7e6f678db199ffdc4ae78c671641
seunope <seunope2004@yahoo.co.uk>
2023-10-30 18:15:21 +0100

    setupgluestack

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { GluestackUIProvider, Text, Center, Box } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to ssddadadtart working on your app!</Text>
-    </View>
+    <GluestackUIProvider config={config}>
+      {/* <Center>
+        <Text>Open up App.js to start working on your app!</Text>
+      </Center> */}
+      {/* <Box width="100%" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box> */}
+
+      <Box>
+        <Center bg="$primary500" h={200} w={300}>
+          <Text color="white" fontWeight="$bold">
+            This is the center.
+          </Text>
+        </Center>
+      </Box>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
